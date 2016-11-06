62219db1f6cc06467b3b9880086495c97f215ed3
Alyssa Torres <torres.lyssa@gmail.com>
2016-11-06 15:53:34 -0800

    initial formatting tabs

--- app/src/main/java/tv/ourglass/alyssa/absinthe_android/MainTabsActivity.java
+++ app/src/main/java/tv/ourglass/alyssa/absinthe_android/MainTabsActivity.java
@@ -21,61 +21,35 @@ import android.widget.TextView;
 
 public class MainTabsActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
-
-    private String[] tabs = {"TV", "Locations", "Settings"};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_tabs);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        TabsPagerAdapter pagerAdapter = new TabsPagerAdapter(getSupportFragmentManager(),
+                MainTabsActivity.this);
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_tabs, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        if (viewPager != null) {
+            viewPager.setAdapter(pagerAdapter);
         }
 
-        return super.onOptionsItemSelected(item);
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        if (tabLayout != null) {
+            tabLayout.setupWithViewPager(viewPager);
+        }
+
+        // Iterate over all tabs and set the custom view
+        if (tabLayout != null) {
+            for (int i = 0; i < tabLayout.getTabCount(); i++) {
+                TabLayout.Tab tab = tabLayout.getTabAt(i);
+                if (tab != null) {
+                    tab.setCustomView(pagerAdapter.getTabView(i));
+                }
+            }
+        }
     }
 
     /**
@@ -110,35 +84,6 @@ public class MainTabsActivity extends AppCompatActivity {
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return tabs.length;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return tabs[position];
         }
     }
 }
---
+++ app/src/main/java/tv/ourglass/alyssa/absinthe_android/TabsPagerAdapter.java
@@ -0,0 +1,77 @@
+package tv.ourglass.alyssa.absinthe_android;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by alyssa on 11/6/16.
+ */
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+    private Context context;
+    private String[] tabTitles = {"TV", "Locations", "Settings"};
+    private int[] tabIcons = {
+            R.drawable.ic_tv_black_24dp,
+            R.drawable.ic_map_black_24dp,
+            R.drawable.ic_settings_black_24dp
+    };
+
+    public TabsPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return SettingsFragment.newInstance(position);
+        //return MainTabsActivity.PlaceholderFragment.newInstance(position);
+        /*
+        switch (tabTitles[position]) {
+            case "TV":
+                return MainTabsActivity.PlaceholderFragment.newInstance(position);
+            case "Locations":
+                return MainTabsActivity.PlaceholderFragment.newInstance(position);
+            case "Settings":
+                return SettingsFragment.newInstance(position);
+            default:
+                return MainTabsActivity.PlaceholderFragment.newInstance(5);
+        }*/
+    }
+
+    @Override
+    public int getCount() {
+        return tabTitles.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        Drawable image = ContextCompat.getDrawable(context, tabIcons[position]);
+        image.setBounds(0, 0, image.getIntrinsicWidth(), image.getIntrinsicHeight());
+        SpannableString sb = new SpannableString(" ");
+        ImageSpan imageSpan = new ImageSpan(image, ImageSpan.ALIGN_BOTTOM);
+        sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return sb;
+    }
+
+    public View getTabView(int position) {
+        // Given you have a custom layout in `res/layout/custom_tab.xml` with a TextView and ImageView
+        View v = LayoutInflater.from(context).inflate(R.layout.custom_tab, null);
+        TextView tv = (TextView) v.findViewById(R.id.text);
+        tv.setText(tabTitles[position]);
+        ImageView img = (ImageView) v.findViewById(R.id.image);
+        img.setImageResource(tabIcons[position]);
+        return v;
+    }
+}
