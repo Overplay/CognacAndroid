54dd22b9a9895d7d6c768cd6a61994954a648b11
Alyssa Torres <torres.lyssa@gmail.com>
2016-11-07 12:12:48 -0800

    adding actions to settings options

--- app/src/main/java/tv/ourglass/alyssa/absinthe_android/SettingsFragment.java
+++ app/src/main/java/tv/ourglass/alyssa/absinthe_android/SettingsFragment.java
@@ -1,13 +1,11 @@
 package tv.ourglass.alyssa.absinthe_android;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
--- app/src/main/java/tv/ourglass/alyssa/absinthe_android/SettingsListAdapter.java
+++ app/src/main/java/tv/ourglass/alyssa/absinthe_android/SettingsListAdapter.java
@@ -2,6 +2,7 @@ package tv.ourglass.alyssa.absinthe_android;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
  */
 
 public class SettingsListAdapter extends ArrayAdapter<SettingsListOption> {
+
     Context context;
 
     public SettingsListAdapter(Context context, ArrayList<SettingsListOption> options) {
@@ -32,13 +34,52 @@ public class SettingsListAdapter extends ArrayAdapter<SettingsListOption> {
         }
 
         ImageView icon = (ImageView) view.findViewById(R.id.icon);
-        icon.setImageResource(option.iconId);
+        if (option != null) {
+            icon.setImageResource(option.iconId);
+        }
 
         TextView name = (TextView) view.findViewById(R.id.name);
-        name.setText(option.name);
+        if (option != null) {
+            name.setText(option.name);
+        }
 
         Typeface font = Typeface.createFromAsset(context.getAssets(), "Poppins-Regular.ttf");
         name.setTypeface(font);
+
+        view.setTag(option);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SettingsListOption option = (SettingsListOption) view.getTag();
+
+                switch (option.name) {
+                    case "Invite Friends":
+                        Log.d("SettingsListAdapter", "invite friends");
+                        break;
+
+                    case "Edit Account":
+                        Log.d("SettingsListAdapter", "edit account");
+                        break;
+
+                    case "Add New Ourglass Device":
+                        Log.d("SettingsListAdapter", "add new device");
+                        break;
+
+                    case "Add/Manage Venues":
+                        Log.d("SettingsListAdapter", "add/manage venues");
+                        break;
+
+                    case "Log Out":
+                        Log.d("SettingsListAdapter", "Log out");
+                        break;
+
+                    default:
+                        Log.d("SettingsListAdapter", "unknown option");
+                        break;
+                }
+            }
+        });
 
         return view;
 
